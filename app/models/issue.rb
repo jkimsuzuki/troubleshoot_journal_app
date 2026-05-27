fd18ecdd77c93a17ac3ee8ed3934846a18e7cbad
jkimsuzuki <161112142+jkimsuzuki@users.noreply.github.com>
2026-05-27 15:47:37 +0900

    Add issue tage associations

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,9 @@
 class Issue < ApplicationRecord
   belongs_to :project
+
+  has_many :issue_tags, dependent: :destroy
+  has_many :tags, through: :issue_tags
+
   validates :title, presence: true
   validates :project_id, presence: true
   validates :status, presence: true
