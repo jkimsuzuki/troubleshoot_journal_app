0e4a6c06a2c79c98a27bc65a72048e3aa514a32f
jkimsuzuki <161112142+jkimsuzuki@users.noreply.github.com>
2026-05-29 01:07:40 +0900

    Style dashboard panels

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,12 @@ Rails.application.routes.draw do
   get "pages/reports"
   get "pages/search"
   get "pages/tags"
-  root "issues#index"
-  
-  
+
+
   get "timeline", to: "pages#timeline"
   get "reports", to: "pages#reports"
   get "search", to: "pages#search"
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
