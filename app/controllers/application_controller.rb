506524cfcdd503405cdb58a8de712bbe88f605c9
jkimsuzuki <161112142+jkimsuzuki@users.noreply.github.com>
2026-06-09 00:01:57 +0900

    Add authentication and fix tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :require_basic_auth, if: -> { Rails.env.production? }
-
-  private
-
-  def require_basic_auth
-    authenticate_or_request_with_http_basic("Troubleshooting Journal") do |username, password|
-      username == ENV["APP_USERNAME"] &&
-        password == ENV["APP_PASSWORD"]
-    end
-  end
+  include Authentication
 end
--- test/controllers/tags_controller_test.rb
+++ test/controllers/tags_controller_test.rb
@@ -1,13 +1,20 @@
 require "test_helper"
 
 class TagsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    @tag = tags(:one)
+
+    sign_in_as @user
+  end
+
   test "should get index" do
     get tags_url
     assert_response :success
   end
 
   test "should get show" do
-    get tag_url(tags(:one))
+    get tag_url(@tag)
     assert_response :success
   end
 end
