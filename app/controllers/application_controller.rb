bb28763396cb5473e7c531c14171af67ad54a5f4
jkimsuzuki <161112142+jkimsuzuki@users.noreply.github.com>
2026-06-08 21:31:54 +0900

    Add production basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  before_action :require_basic_auth, if: -> { Rails.env.production? }
 
-  # Changes to the importmap will invalidate the etag for HTML responses
-  stale_when_importmap_changes
+  private
+
+  def require_basic_auth
+    authenticate_or_request_with_http_basic("Troubleshooting Journal") do |username, password|
+      username == ENV["APP_USERNAME"] &&
+        password == ENV["APP_PASSWORD"]
+    end
+  end
 end
